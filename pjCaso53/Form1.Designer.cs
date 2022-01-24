5e284b231c5fd3a6830c14453b3ac52832b0d5cc
Roger <rogerandresgomezdavila@gmail.com>
2022-01-23 19:14:52 -0600

    Agregar archivos de proyecto.

---
+++ pjCaso53/Form1.Designer.cs
@@ -0,0 +1,304 @@
+﻿namespace pjCaso53
+{
+    partial class frmProformas
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmProformas));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtRazonSocial = new System.Windows.Forms.TextBox();
+            this.txtCantidad = new System.Windows.Forms.TextBox();
+            this.cboTipo = new System.Windows.Forms.ComboBox();
+            this.btnRegistrar = new System.Windows.Forms.Button();
+            this.lvProforma = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.lvEstadisticas = new System.Windows.Forms.ListView();
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader6 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.tsGeneral = new System.Windows.Forms.ToolStripButton();
+            this.tsAnular = new System.Windows.Forms.ToolStripButton();
+            this.tsSalir = new System.Windows.Forms.ToolStripButton();
+            this.groupBox1.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(236, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(261, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "CONTROL DE PROFORMA DE SEGUROS DE VIDA";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(31, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(86, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "RAZON SOCIAL";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 25);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(99, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "TIPO DE SEGURO";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(337, 25);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(108, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "CANT. EMPLEADOS";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(31, 338);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(84, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "ESTADISTICAS";
+            // 
+            // txtRazonSocial
+            // 
+            this.txtRazonSocial.Location = new System.Drawing.Point(27, 70);
+            this.txtRazonSocial.Name = "txtRazonSocial";
+            this.txtRazonSocial.Size = new System.Drawing.Size(674, 20);
+            this.txtRazonSocial.TabIndex = 5;
+            // 
+            // txtCantidad
+            // 
+            this.txtCantidad.Location = new System.Drawing.Point(340, 53);
+            this.txtCantidad.Name = "txtCantidad";
+            this.txtCantidad.Size = new System.Drawing.Size(123, 20);
+            this.txtCantidad.TabIndex = 6;
+            // 
+            // cboTipo
+            // 
+            this.cboTipo.FormattingEnabled = true;
+            this.cboTipo.Items.AddRange(new object[] {
+            "Inversion Clasica",
+            "Inversion Platino",
+            "Inversion Oro"});
+            this.cboTipo.Location = new System.Drawing.Point(9, 53);
+            this.cboTipo.Name = "cboTipo";
+            this.cboTipo.Size = new System.Drawing.Size(293, 21);
+            this.cboTipo.TabIndex = 7;
+            // 
+            // btnRegistrar
+            // 
+            this.btnRegistrar.Location = new System.Drawing.Point(564, 41);
+            this.btnRegistrar.Name = "btnRegistrar";
+            this.btnRegistrar.Size = new System.Drawing.Size(94, 43);
+            this.btnRegistrar.TabIndex = 8;
+            this.btnRegistrar.Text = "REGISTRAR";
+            this.btnRegistrar.UseVisualStyleBackColor = true;
+            this.btnRegistrar.Click += new System.EventHandler(this.btnRegistrar_Click);
+            // 
+            // lvProforma
+            // 
+            this.lvProforma.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3});
+            this.lvProforma.GridLines = true;
+            this.lvProforma.HideSelection = false;
+            this.lvProforma.Location = new System.Drawing.Point(34, 217);
+            this.lvProforma.Name = "lvProforma";
+            this.lvProforma.Size = new System.Drawing.Size(643, 108);
+            this.lvProforma.TabIndex = 9;
+            this.lvProforma.UseCompatibleStateImageBehavior = false;
+            this.lvProforma.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "TIPO DE SEGURO";
+            this.columnHeader1.Width = 332;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "CANT. EMPLEADOS";
+            this.columnHeader2.Width = 157;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "PAGO MENSUAL";
+            this.columnHeader3.Width = 137;
+            // 
+            // lvEstadisticas
+            // 
+            this.lvEstadisticas.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader4,
+            this.columnHeader5,
+            this.columnHeader6});
+            this.lvEstadisticas.GridLines = true;
+            this.lvEstadisticas.HideSelection = false;
+            this.lvEstadisticas.Location = new System.Drawing.Point(37, 369);
+            this.lvEstadisticas.Name = "lvEstadisticas";
+            this.lvEstadisticas.Size = new System.Drawing.Size(639, 117);
+            this.lvEstadisticas.TabIndex = 10;
+            this.lvEstadisticas.UseCompatibleStateImageBehavior = false;
+            this.lvEstadisticas.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "CONCEPTO";
+            this.columnHeader4.Width = 355;
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "TOTAL";
+            // 
+            // columnHeader6
+            // 
+            this.columnHeader6.Text = "";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnRegistrar);
+            this.groupBox1.Controls.Add(this.cboTipo);
+            this.groupBox1.Controls.Add(this.txtCantidad);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Location = new System.Drawing.Point(19, 109);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(667, 95);
+            this.groupBox1.TabIndex = 12;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Registro de seguros";
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsGeneral,
+            this.tsAnular,
+            this.tsSalir});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 509);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(800, 25);
+            this.toolStrip1.TabIndex = 13;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // tsGeneral
+            // 
+            this.tsGeneral.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tsGeneral.Image = ((System.Drawing.Image)(resources.GetObject("tsGeneral.Image")));
+            this.tsGeneral.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsGeneral.Name = "tsGeneral";
+            this.tsGeneral.Size = new System.Drawing.Size(23, 22);
+            this.tsGeneral.Text = "GENERAL";
+            this.tsGeneral.Click += new System.EventHandler(this.tsGeneral_Click);
+            // 
+            // tsAnular
+            // 
+            this.tsAnular.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tsAnular.Image = ((System.Drawing.Image)(resources.GetObject("tsAnular.Image")));
+            this.tsAnular.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsAnular.Name = "tsAnular";
+            this.tsAnular.Size = new System.Drawing.Size(23, 22);
+            this.tsAnular.Text = "ANULAR PROFORMA";
+            this.tsAnular.Click += new System.EventHandler(this.tsAnular_Click);
+            // 
+            // tsSalir
+            // 
+            this.tsSalir.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tsSalir.Image = ((System.Drawing.Image)(resources.GetObject("tsSalir.Image")));
+            this.tsSalir.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsSalir.Name = "tsSalir";
+            this.tsSalir.Size = new System.Drawing.Size(23, 22);
+            this.tsSalir.Text = "SALIR";
+            this.tsSalir.Click += new System.EventHandler(this.tsSalir_Click);
+            // 
+            // frmProformas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 534);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.lvEstadisticas);
+            this.Controls.Add(this.lvProforma);
+            this.Controls.Add(this.txtRazonSocial);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "frmProformas";
+            this.Text = "Planilla de sueldos";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtRazonSocial;
+        private System.Windows.Forms.TextBox txtCantidad;
+        private System.Windows.Forms.ComboBox cboTipo;
+        private System.Windows.Forms.Button btnRegistrar;
+        private System.Windows.Forms.ListView lvProforma;
+        private System.Windows.Forms.ListView lvEstadisticas;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.ColumnHeader columnHeader6;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton tsGeneral;
+        private System.Windows.Forms.ToolStripButton tsAnular;
+        private System.Windows.Forms.ToolStripButton tsSalir;
+    }
+}
+
